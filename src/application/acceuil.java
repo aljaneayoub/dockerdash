fe292b1c3df86075a846b354afc09a8128c240a6
aljaneayoub <ayoub.aljane@tek-up.de>
2023-04-22 18:43:36 +0100

    create cards and borders

--- src/application/acceuil.java
+++ src/application/acceuil.java
@@ -1,5 +1,27 @@
 package application;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 public class acceuil {
+	@FXML
+    private ImageView imageView;
 
+    @FXML
+    private Label titleLabel;
+
+    @FXML
+    private Label descriptionLabel;
+
+    public void setTitle(String title) {
+        titleLabel.setText(title);
+    }
+
+    public void setDescription(String description) {
+        descriptionLabel.setText(description);
+    }
+
+    public void setImage(String imageUrl) {
+        imageView.setImage(new Image(imageUrl));
+    }
 }
