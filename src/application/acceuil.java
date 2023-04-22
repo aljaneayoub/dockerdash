18b3082f4ff955954cc979e37fef03516c12eab6
aljaneayoub <ayoub.aljane@tek-up.de>
2023-04-22 19:02:32 +0100

    prepare desk page

--- src/application/acceuil.java
+++ src/application/acceuil.java
@@ -3,6 +3,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.*;
 public class acceuil {
 	@FXML
     private ImageView imageView;
